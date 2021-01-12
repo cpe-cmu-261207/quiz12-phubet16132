3e8718afc1000619188ed520ba4d53b36ec96828
phubet16132 <59276795+phubet16132@users.noreply.github.com>
2021-01-12 14:00:40 +0700

    half

--- app_template.js
+++ app_template.js
@@ -8,7 +8,7 @@ let myID = 620610804
 let newID = 0
 
 modifyID.addEventListener("click",function(){
-    newID = myID + inputID
+    newID = myID + parseInt(inputID.value)
     const newelement = document.createElement("li")
     newelement.textContent = newID
     List.append(newelement)
