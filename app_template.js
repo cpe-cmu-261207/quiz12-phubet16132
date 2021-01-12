24c5a108fe62b1738b8da624b9f2f08b804093b7
phubet16132 <59276795+phubet16132@users.noreply.github.com>
2021-01-12 13:57:37 +0700

    test1

--- app_template.js
+++ app_template.js
@@ -1,1 +1,15 @@
 //your code here!
+
+let inputID = document.getElementById("input-number")
+let List = document.getElementById("list")
+const modifyID = document.getElementById("button-compute")
+
+let myID = 620610804
+let newID = 0
+
+modifyID.addEventListener("click",function(){
+    newID = myID + inputID
+    const newelement = document.createElement("li")
+    newelement.textContent = newID
+    List.append(newelement)
+})
